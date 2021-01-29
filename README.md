d0b347b3cbe54d71fe747c0ba8e3939f40769e3c
alisafaya <alisafaya@gmail.com>
2021-01-29 18:59:28 +0300

    reorganize

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 
-Augment word list script: 
+## Augment
 
-- takes a file that contains a list of words (one per line), and augments it using wordnet and word-vectors. 
-- output file format is comma separated words at each line
+- Take a text file that contains a list of words (one word per line), and augment this list using wordnet and word-vectors. 
+- If `--output-file` is provided, write that list into the file
+
+__Example usage__
+
+`python augment.py input_sample.txt --output-file output_sample.txt`
+
+__Options__
+
+```bash
+usage: augment.py [-h] [--word-vectors-threshold WORD_VECTORS_THRESHOLD] [--word-net-threshold WORD_NET_THRESHOLD] [--input-file INPUT_FILE] [--output-file OUTPUT_FILE]
+                  [--vector-set VECTOR_SET] [--only-word-net] [--only-word-vectors] [--aggregate]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --word-vectors-threshold WORD_VECTORS_THRESHOLD
+  --word-net-threshold WORD_NET_THRESHOLD
+  --input-file INPUT_FILE
+  --output-file OUTPUT_FILE
+  --vector-set VECTOR_SET
+  --only-word-net
+  --only-word-vectors
+  --aggregate           after augmenting with wordnet, give the augmented list as input to word-vectors augmentation
+
+```
--- augment.py
+++ augment.py
@@ -1,12 +1,13 @@
 import argparse 
 import nltk
 import itertools
+import sys
+
 from tqdm import tqdm
 import gensim.downloader as api
 from nltk.corpus import wordnet as wn
 
-# pip install gensim
-# pip install nltk
+import nltk; nltk.download("wordnet")
 
 def get_words(infile):
     return open(infile).read().splitlines()
@@ -23,9 +24,8 @@ def apply_wordnet(word_list, args):
 def apply_wordvectors(word_list, args):
 
     print("Applying augmentation with word vectors, threshold =", args.word_vectors_threshold)
-    print("Loading vector set")
-
-    model = api.load('word2vec-google-news-300')
+    print("Loading vector set...")
+    model = api.load(args.vector_set)
 
     augmentations = {}
     for w in tqdm(word_list, desc="Extracting similar words"):
@@ -36,28 +36,38 @@ def apply_wordvectors(word_list, args):
 
     return augmentations
 
-def write_output(word_dict, args):
+def write_output(words, args):
     print("Writing output..")
-    with open(args.output_file, "w") as fo:
-        for w in word_dict:
-            print(w + "," + ",".join(word_dict[w]), file=fo)
+    
+    if args.output_file == "stdout":
+        fo = sys.stdout
+        print("\n".join(words), file=fo)
+    else:
+        fo = open(args.output_file, "w")
+        print("\n".join(words), file=fo)
+        fo.close()
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--input-file", type=str, required=True)
+    parser.add_argument("--output-file", type=str, default="stdout")
+
     parser.add_argument("--word-vectors-threshold", type=int, default=10)
     parser.add_argument("--word-net-threshold", type=int, default=10)
-
-    parser.add_argument("--input-file", type=str, default="input_sample.txt")
-    parser.add_argument("--output-file", type=str, default="augment_output.txt")
     
+    parser.add_argument("--vector-set", type=str, default="glove-twitter-25")
     parser.add_argument("--only-word-net", action="store_true")
     parser.add_argument("--only-word-vectors", action="store_true")
     parser.add_argument("--aggregate", action="store_true", help="after augmenting with wordnet, give the augmented list as input to word-vectors augmentation")
 
     args = parser.parse_args()
+
+    print("-"*20)
+    print("Word augmenting tool")
+    print("-"*20)
 
     word_list = get_words(args.input_file)
     word_augmentations = { w : [] for w in word_list }
@@ -65,19 +75,21 @@ if __name__ == '__main__':
     if not args.only_word_vectors:
         augmentations = apply_wordnet(word_list, args)
         for w in augmentations:
-            word_augmentations[w] = list(set(word_augmentations[w] + augmentations[w]))
+            word_augmentations[w] = list(set(word_augmentations.get(w, []) + augmentations[w]))
 
     if not args.only_word_net:
         if args.aggregate:
             print("Extracting word-vectors from augmented list")
-            augmentations = apply_wordvectors(word_list, args)
-        else:
-            augmentations = apply_wordvectors(word_list, args)
+            word_list = word_augmentations.values()
 
+        augmentations = apply_wordvectors(word_list, args)
         for w in augmentations:
-            word_augmentations[w] = list(set(word_augmentations[w] + augmentations[w]))
+            word_augmentations[w] = list(set(word_augmentations.get(w, []) + augmentations[w]))
 
-    write_output(word_augmentations, args)
+    words = list(set( itertools.chain(*[ [w,] + word_augmentations[w] for w in word_augmentations ])))
+    write_output(words, args)
+    
+    print("-"*100)
     print("Finished.")
 
 
